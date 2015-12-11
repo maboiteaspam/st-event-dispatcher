6f676fbb2a650d4e93041d48111da35e852ddd88
Clement Auger <clement.ager@d1m.cn>
2015-12-11 12:11:09 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,35 @@
+# Standard Event Interface for PHP
+
+PSR is cool, but they kept a 2 years old ticket to define a standard eventing system.
+
+https://github.com/php-fig/fig-standards/issues/44
+
+Let s just make it happen..
+
+## Dependencies
+
+All the work of this repo is from symfony repository. It s their code,
+it s just they implements the wrong namespace
+to let a standard occur.
+
+## Install
+
+The recommended way to install ComponentName is through composer.
+
+```json
+{
+    "require": {
+        "st/event-dispatcher": "dev-master#v.1.0.1"
+    }
+}
+```
+
+# see also
+
+The original repo that i basically repackaged.
+
+- https://github.com/symfony/event-dispatcher
+
+A concrete standard implementation to consume in your projects
+
+- https://github.com/maboiteaspam/im-event-dispatcher
---
+++ src/st/Event/EventInterface.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace st\Event;
+/**
+ * Event is the base class for classes containing event data.
+ *
+ * This class contains no event data. It is used by events that do not pass
+ * state information to an event handler when an event is raised.
+ *
+ * You can call the method stopPropagation() to abort the execution of
+ * further listeners in your event listener.
+ *
+ * @author Guilherme Blanco <guilhermeblanco@hotmail.com>
+ * @author Jonathan Wage <jonwage@gmail.com>
+ * @author Roman Borschel <roman@code-factory.org>
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+interface EventInterface
+{
+    /**
+     * Returns whether further event listeners should be triggered.
+     *
+     * @see Event::stopPropagation()
+     *
+     * @return bool Whether propagation was already stopped for this event.
+     */
+    public function isPropagationStopped();
+    /**
+     * Stops the propagation of the event to further event listeners.
+     *
+     * If multiple event listeners are connected to the same event, no
+     * further event listener will be triggered once any trigger calls
+     * stopPropagation().
+     */
+    public function stopPropagation();
+}
